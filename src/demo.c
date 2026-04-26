32f1b9a10368dd1a6fe33509f161f5ab6e140305
Ilkka Hänninen <ilkka.hanninen@gmail.com>
2026-04-26 13:22:18 +0300

    demo: Add texture cube to timeline; shorter scenes

--- src/demo.c
+++ src/demo.c
@@ -5,6 +5,7 @@
 #include "scenes/model_wireframe.h"
 #include "scenes/plasma.h"
 #include "scenes/polyhedra.h"
+#include "scenes/textured_cube.h"
 #include "scenes/tunnel.h"
 #include "utils/timing.h"
 
@@ -13,12 +14,13 @@
 #define PATTERN_LEN 128
 
 TimelineEntry demo_timeline[] = {
-    {&model_wireframe_scene, XM_MS(BPM, SPEED, PATTERN_LEN * 4)},
-    {&model_flatshade_scene, XM_MS(BPM, SPEED, PATTERN_LEN * 4)},
-    {&model_gouraud_scene, XM_MS(BPM, SPEED, PATTERN_LEN * 4)},
-    {&polyhedra_scene, XM_MS(BPM, SPEED, PATTERN_LEN * 4)},
-    {&plasma_scene, XM_MS(BPM, SPEED, PATTERN_LEN * 4)},
-    {&tunnel_scene, XM_MS(BPM, SPEED, PATTERN_LEN * 4)},
+    {&model_wireframe_scene, XM_MS(BPM, SPEED, PATTERN_LEN * 2)},
+    {&model_flatshade_scene, XM_MS(BPM, SPEED, PATTERN_LEN * 2)},
+    {&model_gouraud_scene, XM_MS(BPM, SPEED, PATTERN_LEN * 2)},
+    {&textured_cube_scene, XM_MS(BPM, SPEED, PATTERN_LEN * 2)},
+    {&polyhedra_scene, XM_MS(BPM, SPEED, PATTERN_LEN * 2)},
+    {&plasma_scene, XM_MS(BPM, SPEED, PATTERN_LEN * 2)},
+    {&tunnel_scene, XM_MS(BPM, SPEED, PATTERN_LEN * 2)},
     {0, 0, 0}};
 
 const Asset *demo_song(void)
